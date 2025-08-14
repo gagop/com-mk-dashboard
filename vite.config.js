42c7438c721f85e259bd1f9ad88635ce07eeecb0
Piotr Gago <gagopiotr@gmail.com>
2025-08-14 11:51:12 +0200

    feat: changed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  base: '/bttr-agents/', // only for project pages
   plugins: [svelte()],
   server: {
     port: 5173,
